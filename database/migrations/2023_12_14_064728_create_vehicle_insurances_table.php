8d6d820d1b29fcc9ce991499410d00a2c7567f17
LUC1F3R-99 <146915452+LUC1F3R-99@users.noreply.github.com>
2023-12-14 13:05:50 +0530

    created a fresh laravel project

---
+++ database/migrations/2023_12_14_064728_create_vehicle_insurances_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicle_insurances', function (Blueprint $table) {
+            $table->id();
+            $table->string('vehicleId');
+            $table->string('insuranceId');
+            $table->string('company');
+            $table->string('isActive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicle_insurances');
+    }
+};
