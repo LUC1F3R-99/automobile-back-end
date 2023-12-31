e3f07d2a9854ca0692e4cd29955b3781539d4545
SalindaGIT <salindakas@gmail.com>
2023-12-31 13:35:33 +0530

    Update home form

---
+++ resources/views/home.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- included csrf for protection --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Automobile System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <form action="#" method="POST" id="detailsForm">
+        @csrf
+        <div class="container mt-5">
+            <div class="card">
+                <div class="card-body">
+                    <div class="mb-3">
+                        {{-- Vehicle no search field --}}
+                        <label for="searchVehicleNo" class="form-label">Search Vehicle No</label>
+                        <input type="text" class="form-control" id="searchvehicleNumber" name="searchVehicleNo"
+                            placeholder="AB x x x x or ABC x x x x">
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- Customer details --}}
+        <div class="container mt-5">
+            <div class="card">
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="customerId" class="form-label">Customer ID</label>
+                        <input type="text" class="form-control" id="customerId" name="customerId"
+                            placeholder="Customer ID" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="customerName" class="form-label">Customer Name</label>
+                        <input type="text" class="form-control editable-field" id="customerName" name="customerName"
+                            placeholder="john Doe" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="contactNo" class="form-label">Contact Number </label>
+                        <input type="text" class="form-control editable-field" id="contactNo" name="contactNo"
+                            placeholder="07x xxx.." disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="nic" class="form-label">NIC</label>
+                        <input type="text" class="form-control editable-field" id="nic" name="nic"
+                            placeholder="9xx..." disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="address" class="form-label">Address</label>
+                        <input type="text" class="form-control editable-field" id="address" name="address"
+                            placeholder="Noxx Colombo xx" disabled>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- vehicle details  --}}
+        <div class="container mt-5">
+            <div class="card">
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="vehicleNumber" class="form-label">Vehicle Number</label>
+                        <input type="text" class="form-control editable-field" id="vehicleNumber"
+                            name="vehicleNumber" placeholder="AB x x x x or ABC x x x x" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="make" class="form-label">Make</label>
+                        <input type="text" class="form-control editable-field" id="make" name="make"
+                            placeholder="Toyota" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="model" class="form-label">Model</label>
+                        <input type="text" class="form-control editable-field" id="model" name="model"
+                            placeholder="Carina" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="year" class="form-label">Year</label>
+                        <input type="text" class="form-control editable-field" id="year" name="year"
+                            placeholder="2024" disabled>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- Insurance details  --}}
+        <div class="container mt-5">
+            <div class="card">
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="insuranceNo" class="form-label">Insurance Policy Number</label>
+                        <input type="text" class="form-control editable-field" id="insuranceNo"
+                            name="insuranceNo" placeholder="000123456" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="company" class="form-label">Insurance Company</label>
+                        <input type="text" class="form-control editable-field" id="insuranceCompany"
+                            name="company" placeholder="Continental" disabled>
+                    </div>
+                    {{-- Buttons  --}}
+                    <div class="mb-3" id="buttonGroup">
+                        <button type="button" class="btn btn-secondary" id="editButton">Edit</button>
+                        <button type="button" class="btn btn-success" id="invoiceButton">Go to
+                            Invoice</button>
+                        <button type="button" class="btn btn-danger" id="resetButton">Reset</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </form>
+
+
+    <!-- link jqeury  -->
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+        crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/homeView', function () {
+    return view('home');
+});
 Route::get('/', function () {
     return view('welcome');
 });
